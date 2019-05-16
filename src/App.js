3ac68648603a3283413499ddb97c6b81a0c5d449
Zhiqiang du <haxck.x@gmail.com>
2019-05-16 19:39:10 +0800

    init studio index

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Logo from './components/Logo'
+import Projects from './components/Projects'
+
+function App() {
+  return (
+    <div className="App">
+      <Logo />
+      <Projects />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Logo.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Logo.css'
+
+function Logo() {
+    return (
+        <div className="hero">
+            <div className="logo">
+                <div className="cross-left"></div>
+                <div className="cross-right"></div>
+            </div>
+            <p className="text">CREATE A NEW WORLD</p>
+        </div>
+    )
+}
+
+export default Logo
