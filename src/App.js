85ac1e82a316973621af343871da77239203fd6b
Zhiqiang du <haxck.x@gmail.com>
2019-05-20 08:51:33 +0800

    add footer in app.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Logo from './components/Logo'
 import Projects from './components/Projects'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <div className="App">
       <Logo />
       <Projects />
+      <Footer />
     </div>
   );
 }
