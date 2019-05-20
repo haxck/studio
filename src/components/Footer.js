44ed9f584a6d20fae18cc588196184f66908ae9f
Zhiqiang du <haxck.x@gmail.com>
2019-05-20 08:51:12 +0800

    footer

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../css/tailwind.css'
+
+class Footer extends React.Component{
+    render() {
+        return (
+            <footer className="w-full text-center py-3 bg-black text-white">
+                <p>Design by haxck</p>
+            </footer>
+        )
+    }
+}
+
+export default Footer
