1abb2d3306e6acd59e360d1d9dffa79c9e1c51ce
Zhiqiang du <haxck.x@gmail.com>
2019-06-06 19:42:59 +0800

    switch to function component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,26 +1,19 @@
 import React from 'react'
 import '../css/tailwind.css'
 
-class Card extends React.Component {
-    constructor(props) {
-        super(props)
-    }
-    render() {
-        return (
-            <div className="p-4 border-box w-full md:w-1/3">
-                <div className={"w-full rounded " + (this.props.color || "bg-blue-400")}>
-                    <div className="card-header"></div>
-                    <div className="card-body">
-                        <h1 className="py-4 px-5 text-4xl text-white">{this.props.title}</h1>
-                        <h4 className="pb-8 px-5 text-xl text-white">{this.props.content}</h4>
-                    </div>
-                    <div className="card-footer p-2 rounded-b bg-black opacity-25 clearfix">
-                        <a href={this.props.link} className="float-right py-1 text-white font-bold">{this.props.linkName}</a>
-                    </div>
+export default function Card(props) {
+    return (
+        <div className="p-4 border-box w-full md:w-1/3">
+            <div className={"w-full rounded " + (props.color || "bg-blue-400")}>
+                <div className="card-header"></div>
+                <div className="card-body">
+                    <h1 className="py-4 px-5 text-4xl text-white">{props.title}</h1>
+                    <h4 className="pb-8 px-5 text-xl text-white">{props.content}</h4>
+                </div>
+                <div className="card-footer p-2 rounded-b bg-black opacity-25 clearfix">
+                    <a href={props.link} className="float-right py-1 text-white font-bold">{props.linkName}</a>
                 </div>
             </div>
-        )
-    }
+        </div>
+    )
 }
-
-export default Card;
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,10 @@
 import React from 'react'
 import '../css/tailwind.css'
 
-class Footer extends React.Component{
-    render() {
-        return (
-            <footer className="w-full text-center py-5 bg-black text-gray-500">
-                <p>Design by haxck</p>
-            </footer>
-        )
-    }
+export default function Footer(){
+    return (
+        <footer className="w-full text-center py-5 bg-black text-gray-500">
+            <p>Design by haxck</p>
+        </footer>
+    )
 }
-
-export default Footer
