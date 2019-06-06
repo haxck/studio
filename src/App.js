025bc6ab4a002e8342d129a2932989a1ae0ce206
Zhiqiang du <haxck.x@gmail.com>
2019-06-06 19:35:44 +0800

    split data in components

--- src/App.js
+++ src/App.js
@@ -4,11 +4,17 @@ import Logo from './components/Logo'
 import Projects from './components/Projects'
 import Footer from './components/Footer'
 
+
+
 function App() {
+  const data = [
+    { title: "Echo", content: "书信风格长文本生成器。", link: "https://github.com/haxck/echo", linkName: "Github" },
+    { title: "NightMate", content: "白噪音小程序。困意来袭，无需选择，一键开始入睡。辗转反侧，夜读陪你漫漫长夜。", link: "https://haxck.com/lite-program-nightmate/", linkName: "查看详情" },
+  ]
   return (
     <div className="App">
       <Logo />
-      <Projects />
+      <Projects data={data} />
       <Footer />
     </div>
   );
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,13 +3,18 @@ import '../css/tailwind.css'
 import Card from './Card'
 
 
-function Projects() {
+
+function Projects({ data }) {
+
   return (
     <div className="container flex flex-wrap mx-auto pb-5">
-      <Card color="bg-blue-400" title="Echo" content="书信风格长文本生成器。 " link="http://haxck.com" linkName="查看项目"></Card>
-      <Card color="bg-green-400" title="NightMate" content="白噪音小程序。困意来袭，无需选择，一键开始入睡。辗转反侧，夜读陪你漫漫长夜。" link="http://haxck.com" linkName="查看项目"></Card>
+      {
+        data.map((props) => {
+          return <Card {...props} />
+        })
+      }
     </div>
-)
+  )
 }
 
 export default Projects
